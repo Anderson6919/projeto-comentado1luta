1c46b24acc7c27ff5e4340fbefd295fe8c9c62b4
Anderson6919 <159493613+Anderson6919@users.noreply.github.com>
2024-08-28 16:28:21 -0300

    Add files via upload

---
+++ src/components/heros/Mage.js
@@ -0,0 +1,9 @@
+class Mage extends Character {
+    constructor(name){
+        super(name)
+        this.life = 5
+        this.atack = 15
+        this.defense = 3
+        this.maxLife = this.life
+    }
+}
---
+++ src/components/monsters/LittleMonster.js
@@ -0,0 +1,9 @@
+class LittleMonster extends Character {
+    constructor(){
+        super('LittleMonster')
+        this.life = 1 
+        this.atack = 1 
+        this.defense = 1 
+        this.maxLife = this.life
+    }
+}
