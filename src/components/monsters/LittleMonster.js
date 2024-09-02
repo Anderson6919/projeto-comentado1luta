01dc9a66bb3b5e17cf91d54a8d9d00885c784921
Anderson6919 <159493613+Anderson6919@users.noreply.github.com>
2024-09-02 09:30:30 -0300

    Delete src directory

--- src/components/monsters/LittleMonster.js
+++
@@ -1,9 +0,0 @@
-class LittleMonster extends Character {
-    constructor(){
-        super('LittleMonster')
-        this.life = 1 
-        this.atack = 1 
-        this.defense = 1 
-        this.maxLife = this.life
-    }
-}
